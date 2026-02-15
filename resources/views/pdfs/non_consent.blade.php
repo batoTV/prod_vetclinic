a4ff5f01521c2a4fd4444293225b04cf17ceae79
Ian <ianvirgillim@gmail.com>
2026-02-15 14:45:20 +0800

    variable naming convention fix

--- resources/views/pdfs/non_consent.blade.php
+++ resources/views/pdfs/non_consent.blade.php
@@ -25,7 +25,7 @@
     <table class="details-table">
         <tr>
             <td>
-    <strong>Owner's Name:</strong> {{ $first_Name }} {{ $last_Name }}
+    <strong>Owner's Name:</strong> {{ $first_name }} {{ $last_name }}
 </td>
             <td><strong>Pet's Name:</strong> {{ $petName }}</td>
         </tr>
--- resources/views/pdfs/surgery_consent.blade.php
+++ resources/views/pdfs/surgery_consent.blade.php
@@ -31,7 +31,7 @@
     <table class="details">
         <tr>
             <td>
-    <strong>Owner's Name:</strong> {{ $first_Name }} {{ $last_Name }}
+    <strong>Owner's Name:</strong> {{ $first_name }} {{ $last_name }}
 </td>
             <td><strong>Pet's Name:</strong> {{ $petName }}</td>
         </tr>
