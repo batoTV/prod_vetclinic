bf88507eb67a2e8fbb380eb5cffeca651d72da38
Ian <ianvirgillim@gmail.com>
2026-03-04 17:16:17 +0800

    nonconsent vartiable fix

--- resources/views/pdfs/non_consent.blade.php
+++ resources/views/pdfs/non_consent.blade.php
@@ -25,7 +25,7 @@
     <table class="details-table">
         <tr>
             <td>
-    <strong>Owner's Name:</strong> {{ $first_name }} {{ $last_name }}
+    <strong>Owner's Name:</strong> {{ $firstName }} {{ $lastName }}
 </td>
             <td><strong>Pet's Name:</strong> {{ $petName }}</td>
         </tr>
